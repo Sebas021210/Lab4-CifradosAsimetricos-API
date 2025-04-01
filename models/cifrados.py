61db926e82e9897b4469bd0a637fab0a8b34e697
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-04-01 12:27:36 -0600

    Modelo User

---
+++ models/cifrados.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class User(BaseModel):
+    name: str
+    last_name: str
+    username: str
+    email: str
+    password: str
+    public_key: Optional[str] = None
+    files: Optional[List[str]] = []
+    files_hash: Optional[List[str]] = []
+    files_firma: Optional[List[str]] = []

00f9acdaf51f53f876425222ba5407eeab9eee47
ManuelR11 <rod21509@uvg.edu.gt>
2025-04-09 19:20:37 -0600

    Manejo de Cors

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # 👈 Agregado
 from pymongo.mongo_client import MongoClient
 from routes.route import router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Dominio del frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(router)
